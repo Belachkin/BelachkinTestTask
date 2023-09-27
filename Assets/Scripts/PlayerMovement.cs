8029a2ab819966686e4fa36ff02ea74e9911ed48
Danila <belkin.job123@gmail.com>
2023-09-27 20:12:35 +0300

    add player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private FixedJoystick _joystick;
 
     private Rigidbody2D _rb;
+    private Vector2 _direction;
+
 
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        _direction = _joystick.Direction;
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.MovePosition(_rb.position + _direction * _speed * Time.fixedDeltaTime);
+    }
 }
